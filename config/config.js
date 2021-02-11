9cc79f3fcdad8ed5a55acb4917d74440c754790d
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-02-10 20:06:54 -0800

    Fix database config for production

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgresql"
   }
 }
