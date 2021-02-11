407c6ccf2809d03bbfe82d38b150496797883738
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-02-10 17:21:18 -0800

    Backing up final product before restructuring

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,10 @@ const cors = require("cors")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let apiRouter = require("./routes/api")
+// let apiRouter = require("./routes/api")
 
 var app = express();
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/api", apiRouter)
+// app.use("/api", apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,13 @@ import ParticipantsList from "./participants/ParticipantsList"
 // const backendURL = "http://192.168.228.111:9000/api/"
 // const backendURL = "http://localhost:9000/"
 // const backendURL = "http://192.168.228.111:9000"
-const backendURL = "https://127.0.0.1:3000"
+let backendURL
+if (process.env.NODE_ENV === 'development') {
+	backendURL ='http://localhost:9000/';
+}
+else {
+	backendURL = "/api"
+}
 const socket = io(backendURL)
 
 
