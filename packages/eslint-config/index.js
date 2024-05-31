ce81ce2d7a5b7066a30c8c71c0e7302386d516ca
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-05-31 01:29:50 -0700

    chore: update frontend packages, make frontend work with vite

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tseslint, { parser } from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConf = [
@@ -19,6 +19,8 @@ const eslintConf = [
         linterOptions: {
             reportUnusedDisableDirectives: "error",
         },
+        ignorePatterns: ["dist"],
+        parser: parser,
     },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
