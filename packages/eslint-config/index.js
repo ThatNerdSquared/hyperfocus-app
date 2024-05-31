691a57880fe69190ae07bdd1caf49447ee7ec305
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-05-31 01:14:21 -0700

    chore: add in unified prettier and tsc configs

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,26 +2,28 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConf = [
-  {languageOptions: { globals: globals.browser },
-      rules: {
-          "no-await-in-loop": "warn",
-          "no-duplicate-imports": "error",
-          "no-self-compare": "error",
-          "no-template-curly-in-string": "error",
-          "no-unreachable-loop": "error",
-          "prefer-promise-reject-errors": "error",
-        "yoda": "warn"
-      },
-              linterOptions: {
-            reportUnusedDisableDirectives: "error"
-        }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig
+    {
+        languageOptions: { globals: globals.browser },
+        rules: {
+            "no-await-in-loop": "warn",
+            "no-duplicate-imports": "error",
+            "no-self-compare": "error",
+            "no-template-curly-in-string": "error",
+            "no-unreachable-loop": "error",
+            "prefer-promise-reject-errors": "error",
+            yoda: "warn",
+        },
+        linterOptions: {
+            reportUnusedDisableDirectives: "error",
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReactConfig,
+    eslintConfigPrettier,
 ];
 
 export default eslintConf;
